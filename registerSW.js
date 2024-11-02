c50eef09ac0cd61d90fa3ad58d212e53719b95cd
Shalika-Madhushanki <shali.madhushanki@gmail.com>
2024-11-02 18:40:31 +0100

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/expense-tracker-frontend/sw.js', { scope: '/expense-tracker-frontend/' })})}
